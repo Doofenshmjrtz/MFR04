0eab6284d57d4e8abe992dd71540e8eeb4b31cfc
Doofenshmjrtz <beqagvritishvili@gmail.com>
2021-03-09 16:25:04 +0400

    deleted extra js files

--- deletescr.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#delete").click(function() {
-        $.ajax({
-            type: "POST",
-            url: "delete.php",
-            dataType: "json"
-        });
-    });
-});
--- outputscr.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var userCount=false;
-    $("#usersbtn").click(function(){
-        userCount = true;
-        $("#users").load("useroutput.php", {
-            newUserCount: userCount
-        });
-    });
-});
