b2439629e7eb3647ed89bd720ac29d770a4a86c7
Doofenshmjrtz <beqagvritishvili@gmail.com>
2021-03-15 23:13:40 +0400

    some update

--- users.sql
+++ users.sql
@@ -6,63 +6,60 @@
 -- Generation Time: Mar 09, 2021 at 04:35 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!40101 SET NAMES utf8mb4 */
+;
 --
 -- Database: `signup`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
-
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(31) DEFAULT 'NOT NULL',
   `last_name` varchar(31) DEFAULT 'NOT NULL',
   `email` varchar(31) DEFAULT 'NOT NULL'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 --
 -- Dumping data for table `users`
 --
-
-INSERT INTO `users` (`id`, `name`, `last_name`, `email`) VALUES
-(88, 'beqa', 'gvritishvili', 'beqagvritishvili@gmail.com');
-
+INSERT INTO `users` (`name`, `last_name`, `email`)
+VALUES (
+    'beqa',
+    'gvritishvili',
+    'beqagvritishvili@gmail.com'
+  );
 --
 -- Indexes for dumped tables
 --
-
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
+ADD PRIMARY KEY (`id`);
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
